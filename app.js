96e9c571576e569951dc82e000f8188e39df8419
David Lopez <101904205+deivi2401@users.noreply.github.com>
2024-03-30 00:28:48 -0500

    Se implementaron cambios a la api de aprendizaje
    Se crearon las rutas de orders y de products para ver como funcionan las funcionalidades de get y post

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
-    });
-});
+const productRoutes = require('./routes/products');
+const orderRoutes = require('./routes/orders');
+
+app.use('/orders', orderRoutes);
+app.use('/products', productRoutes);
 
 const port = process.env.port || 3000;
 app.listen(port, () => console.log(`Escuchando en puerto ${port}...`));
